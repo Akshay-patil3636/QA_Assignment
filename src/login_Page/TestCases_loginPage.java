7e1d993b5555f29a151576a7ccc041ce94c1bdac
Akshay-patil3636 <patilakshay9251@gmail.com>
2023-07-15 12:00:57 +0530

    first commit by akshay

---
+++ src/login_Page/TestCases_loginPage.java
@@ -0,0 +1,114 @@
+package login_Page;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class TestCases_loginPage {
+	WebDriver driver;
+	SoftAssert s=new SoftAssert();
+	boolean Status_errormsg = false;
+
+	@BeforeClass
+	public void openBrowser()
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\ALL_Selenium_Jars\\chromedriver.exe");
+		 driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");   // here we are given sample facebook login url
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		
+		driver.manage().window().maximize();
+		
+	}
+	
+//	@Test
+	public void successful_login()
+	{
+		driver.findElement(By.xpath("//input[@name='username' and @placeholder='Username']")).sendKeys("Admin"); // passed valied username
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");  // passed valied password
+	    driver.findElement(By.xpath("//button[@type='submit']")).click();
+	    String Exp_HpgeURl=driver.getCurrentUrl();
+	    String A_Hpgeurl="https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index";
+	    s.assertEquals(A_Hpgeurl, Exp_HpgeURl);
+	    
+	
+	}
+//	@Test
+	public void invaliedCredentials()
+	{
+		driver.findElement(By.xpath("//input[@name='username' and @placeholder='Username']")).sendKeys("dmin"); // passed valied username
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin23");  // passed valied password
+	    driver.findElement(By.xpath("//button[@type='submit']")).click();
+		boolean Status_errormsg= driver.findElement(By.xpath("//div[@class='oxd-alert oxd-alert--error']")).isDisplayed();
+	   System.out.println(Status_errormsg);
+		s.assertTrue(Status_errormsg);
+	s.assertAll();
+	}
+	//@Test(dependsOnMethods ="invaliedCredentials")
+	public void getFailedlogin_credential()
+	{
+		if(Status_errormsg ==true)
+		{
+		String username=driver.findElement(By.xpath("//input[@name='username' and @placeholder='Username']")).getAttribute("placeholder");
+		System.out.println("Wrong credentials provided");	
+		System.out.println("wrong username:"+username);
+		String password=driver.findElement(By.xpath("//input[@name='password']")).getAttribute("placeholder");
+		System.out.println("wrong password:"+password);
+		s.assertAll();
+
+		}
+	}
+//	@Test
+	public void loginBtnDisabled()
+	{
+	 boolean Status_loginBtn=   driver.findElement(By.xpath("//button[@type='submit']")).isEnabled();
+       System.out.println(Status_loginBtn);
+       s.assertFalse(Status_loginBtn);
+		s.assertAll();
+	}
+//	@Test 
+	public void loginBtnEnabled_validCrednetial()
+	{
+		driver.findElement(By.xpath("//input[@name='username' and @placeholder='Username']")).sendKeys("Admin"); // passed valied username
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");  // passed valied password
+		boolean status_loginbtn_valied=  driver.findElement(By.xpath("//button[@type='submit']")).isEnabled();
+	    s.assertTrue(status_loginbtn_valied);
+	    System.out.println(status_loginbtn_valied);
+	    System.out.println("Login btn is displayed after valied credentials");
+		
+		
+	}
+  //    @Test
+      public void redirect_forgetPass()
+      {
+    	  driver.findElement(By.xpath("//p[@class='oxd-text oxd-text--p orangehrm-login-forgot-header']")).click();
+          String Aurl="https://opensource-demo.orangehrmlive.com/web/index.php/auth/requestPasswordResetCode";
+      String Eurl=driver.getCurrentUrl();
+      s.assertEquals(Aurl, Eurl);
+      System.out.println("successfully redirected to forget passwod page");
+      s.assertAll();
+      }
+      
+      @Test
+      public void validate_PassRecovery()
+      {
+    	  driver.findElement(By.xpath("//p[@class='oxd-text oxd-text--p orangehrm-login-forgot-header']")).click();
+    	driver.findElement(By.xpath("//input[@name='username'] [@class='oxd-input oxd-input--active']")).sendKeys("Akshay@123");
+    	driver.findElement(By.xpath("//button[@type='submit']")).click();
+    	
+    boolean passRec_msg=  driver.findElement(By.xpath("//div[@class='orangehrm-card-container']")).isDisplayed();
+       s.assertTrue(passRec_msg);
+       
+       
+      }
+      
+      
+}
