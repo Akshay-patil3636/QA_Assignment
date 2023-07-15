49d59940d1e0b9100f30bd017c700b7aeb5ccb8e
Akshay-patil3636 <patilakshay9251@gmail.com>
2023-07-15 14:17:58 +0530

    second commit by akshay

--- src/login_Page/TestCases_loginPage.java
+++ src/login_Page/TestCases_loginPage.java
@@ -5,8 +5,12 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -15,7 +19,7 @@ public class TestCases_loginPage {
 	SoftAssert s=new SoftAssert();
 	boolean Status_errormsg = false;
 
-	@BeforeClass
+	@BeforeMethod
 	public void openBrowser()
 	{
 		System.setProperty("webdriver.chrome.driver", "D:\\ALL_Selenium_Jars\\chromedriver.exe");
@@ -29,7 +33,7 @@ public class TestCases_loginPage {
 		
 	}
 	
-//	@Test
+	@Test(priority =1)
 	public void successful_login()
 	{
 		driver.findElement(By.xpath("//input[@name='username' and @placeholder='Username']")).sendKeys("Admin"); // passed valied username
@@ -41,7 +45,7 @@ public class TestCases_loginPage {
 	    
 	
 	}
-//	@Test
+	@Test(priority =2)
 	public void invaliedCredentials()
 	{
 		driver.findElement(By.xpath("//input[@name='username' and @placeholder='Username']")).sendKeys("dmin"); // passed valied username
@@ -52,29 +56,32 @@ public class TestCases_loginPage {
 		s.assertTrue(Status_errormsg);
 	s.assertAll();
 	}
-	//@Test(dependsOnMethods ="invaliedCredentials")
+	@Test(priority=3)
 	public void getFailedlogin_credential()
 	{
-		if(Status_errormsg ==true)
-		{
-		String username=driver.findElement(By.xpath("//input[@name='username' and @placeholder='Username']")).getAttribute("placeholder");
+		driver.findElement(By.xpath("//input[@name='username' and @placeholder='Username']")).sendKeys("dmin"); // passed valied username
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin23");  // passed valied password
+	    driver.findElement(By.xpath("//button[@type='submit']")).click();
+		
+		
+	    String username=driver.findElement(By.xpath("//input[@name='username' and @placeholder='Username']")).getAttribute("value");
 		System.out.println("Wrong credentials provided");	
 		System.out.println("wrong username:"+username);
-		String password=driver.findElement(By.xpath("//input[@name='password']")).getAttribute("placeholder");
+		String password=driver.findElement(By.xpath("//input[@name='password']")).getAttribute("value");
 		System.out.println("wrong password:"+password);
 		s.assertAll();
 
-		}
+		
 	}
-//	@Test
+	@Test(priority =4)
 	public void loginBtnDisabled()
 	{
 	 boolean Status_loginBtn=   driver.findElement(By.xpath("//button[@type='submit']")).isEnabled();
        System.out.println(Status_loginBtn);
        s.assertFalse(Status_loginBtn);
-		s.assertAll();
+	   s.assertAll();
 	}
-//	@Test 
+	@Test (priority =5)
 	public void loginBtnEnabled_validCrednetial()
 	{
 		driver.findElement(By.xpath("//input[@name='username' and @placeholder='Username']")).sendKeys("Admin"); // passed valied username
@@ -86,7 +93,7 @@ public class TestCases_loginPage {
 		
 		
 	}
-  //    @Test
+      @Test(priority =6)
       public void redirect_forgetPass()
       {
     	  driver.findElement(By.xpath("//p[@class='oxd-text oxd-text--p orangehrm-login-forgot-header']")).click();
@@ -97,18 +104,25 @@ public class TestCases_loginPage {
       s.assertAll();
       }
       
-      @Test
+     @Test(priority =7)
       public void validate_PassRecovery()
       {
-    	  driver.findElement(By.xpath("//p[@class='oxd-text oxd-text--p orangehrm-login-forgot-header']")).click();
+    	  String Amsg="Reset Password link sent successfully";
+    	driver.findElement(By.xpath("//p[@class='oxd-text oxd-text--p orangehrm-login-forgot-header']")).click();
     	driver.findElement(By.xpath("//input[@name='username'] [@class='oxd-input oxd-input--active']")).sendKeys("Akshay@123");
     	driver.findElement(By.xpath("//button[@type='submit']")).click();
     	
-    boolean passRec_msg=  driver.findElement(By.xpath("//div[@class='orangehrm-card-container']")).isDisplayed();
-       s.assertTrue(passRec_msg);
+      WebElement msg=  driver.findElement(By.xpath("//div[@class='orangehrm-card-container']"));
+     String Emsg=  msg.getText();
+     s.assertEquals(Amsg,Emsg);
+     
        
        
       }
       
-      
+      @AfterMethod
+      public void closeBrowser()
+      {
+    	  driver.quit();
+      }
 }
